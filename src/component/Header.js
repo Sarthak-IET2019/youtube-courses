bcc8cd5b8311f4cdee7abd4c4a42292b4d029f46
Sarthak <sarthakiet2019@gmail.com>
2023-06-21 16:55:20 +0530

    first commit after react initial setup

---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header(){
+
+    return(
+        <div>
+            <h1>Header</h1>
+            <h1>Code with Sarthak</h1>
+            <p>this is a header app</p>
+        </div>
+    );
+}
+
+export default Header;
